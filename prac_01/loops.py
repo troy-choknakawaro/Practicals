5abd9e70f8d2bd239b43cf980ea2d7c4d3f0f589
troy <tanaporn.choknakawaro@my.jcu.edu.au>
2022-08-01 00:55:59 +0700

    add a loop file

---
+++ prac_01/loops.py
@@ -0,0 +1,28 @@
+for i in range(1, 21, 2):
+    print(i, end=' ')
+print()
+
+print('---------')
+
+for i in range(0, 110, 10):
+    print(i, end='  ')
+print()
+
+print('---------')
+
+for i in range(20, 0, -1):
+    print(i, end='  ')
+print()
+
+print('---------')
+
+n = int(input())
+for i in range(n):
+    print('*', end='')
+print()
+
+print('---------')
+
+for i in range(1, n + 1):
+    print('*' * i)
+print()
