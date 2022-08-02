cdae7303f64de5c3da37029121225be8c778f23c
troy <tanaporn.choknakawaro@my.jcu.edu.au>
2022-08-03 00:13:45 +0700

    fix a broken score file

---
+++ prac_03/broken_score.py
@@ -0,0 +1,15 @@
+def main():
+    score = float(input("Enter score: "))
+    print(score_status(score))
+
+def score_status(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+main()
