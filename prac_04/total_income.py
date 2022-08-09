d836c325b356a1f5be263a4d1bcdbda318b3541b
troy <tanaporn.choknakawaro@my.jcu.edu.au>
2022-08-09 23:32:44 +0700

    refactor variables

---
+++ prac_04/total_income.py
@@ -0,0 +1,36 @@
+"""
+CP1404/CP5632 Practical
+Starter code for cumulative total income program
+"""
+
+
+"""
+CP1404/CP5632 Practical
+Starter code for cumulative total income program
+"""
+
+
+def main():
+    """Display income report for incomes over a given number of months."""
+    incomes = []
+    number_of_months = int(input("How many months? "))
+
+    for month in range(1, number_of_months + 1):
+        income = float(input("Enter income for month {}: ".format(month)))
+        incomes.append(income)
+
+    print_report(incomes)
+
+
+def print_report(incomes):
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month, income in enumerate(incomes):
+        income = incomes[month - 1]
+        total += income
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month + 1, income, total))
+
+
+main()
+
+# i look at the solution because i cannot figure it out, but i understand it now
