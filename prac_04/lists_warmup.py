75d6183396f1f1afd222093fc065d73f9ad1a344
troy <tanaporn.choknakawaro@my.jcu.edu.au>
2022-08-09 23:37:34 +0700

    do a list exercise

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -11,22 +11,22 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # numbers + [6, 5, 3] => [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 
 print(numbers)
-print('--------')
+print('-------')
 
 # Change the first element of numbers to "ten" (the string, not the number 10)
 numbers[0] = 'ten'
 print(numbers)
-print('--------')
+print('-------')
 
 # Change the last element of numbers to 1
 numbers[6] = 1
 print(numbers)
-print('--------')
+print('-------')
 
 # Get all the elements from numbers except the first two (slice)
 numbers = numbers[2:]
 print(numbers)
-print('--------')
+print('-------')
 
 # Check if 9 is an element of numbers
 numbers = 9 in numbers
